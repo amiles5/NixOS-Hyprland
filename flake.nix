7d495e62c2f9c1a61df0b085b7544bfd87ec906a
installer <installer@gmail.com>
2025-01-18 10:06:04 +0000

    nitial push

---
+++ flake.nix
@@ -0,0 +1,43 @@
+# 💫 https://github.com/JaKooLit 💫 #
+
+{
+  description = "KooL's NixOS-Hyprland"; 
+  	
+  inputs = {
+  	nixpkgs.url = "nixpkgs/nixos-unstable";
+	#hyprland.url = "github:hyprwm/Hyprland"; # hyprland development
+	distro-grub-themes.url = "github:AdisonCavani/distro-grub-themes";
+	ags.url = "github:aylur/ags/v1"; # aylurs-gtk-shell-v1
+  	};
+
+  outputs = 
+	inputs@{ self, nixpkgs, ... }:
+    	let
+      system = "x86_64-linux";
+      host = "m6";
+      username = "milesj";
+
+    pkgs = import nixpkgs {
+       	inherit system;
+       	config = {
+       	allowUnfree = true;
+       	};
+      };
+    in
+      {
+	nixosConfigurations = {
+      "${host}" = nixpkgs.lib.nixosSystem rec {
+		specialArgs = { 
+			inherit system;
+			inherit inputs;
+			inherit username;
+			inherit host;
+			};
+	   		modules = [ 
+				./hosts/${host}/config.nix 
+				inputs.distro-grub-themes.nixosModules.${system}.default
+				];
+			};
+		};
+	};
+}
